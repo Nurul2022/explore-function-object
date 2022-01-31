a7309712b98dd7ba5fb953089e973979ed0f9361
Nurul2022 <nurulbdpersonal@gmail.com>
2022-01-31 12:36:31 +0600

    function added

---
+++ function.js
@@ -0,0 +1,19 @@
+// function declaretion
+function startFan() {
+    console.log('walk towards the switch')
+    console.log('press the switch');
+
+}
+
+// call the function
+startFan();
+
+console.log('eat breakfast');
+console.log('drink tea');
+console.log('take a shower');
+startFan();
+var country = 'bd';
+var city = 'Dhaka';
+var location = 'Gulshan';
+startFan();
+
