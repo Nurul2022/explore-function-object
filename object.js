8bf36b2523c3664d3051db697c0682ab9287293a
Nurul2022 <nurulbdpersonal@gmail.com>
2022-01-31 19:31:03 +0600

    object added

---
+++ object.js
@@ -0,0 +1,5 @@
+var student1 = {id: 115, name: 'Abir Adnan', marks: 81, phone: 1234567};
+
+var mobile = {color: 'black', price: 19000, screenSize: 5, Storage: '16gb'};
+
+var book = {tittle: 'start with why', author: 'simon panda', price: 200, pages: 246, publisher: 'penguin' }; 
