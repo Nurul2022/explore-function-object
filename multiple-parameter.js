b39e521af7ae512b87216f940113d0e0f7b50fb4
Nurul2022 <nurulbdpersonal@gmail.com>
2022-01-31 18:53:25 +0600

    multiple-parameter added

---
+++ multiple-parameter.js
@@ -0,0 +1,39 @@
+function addTwoNumbers(number1, number2){
+    console.log(number1, number2);
+// console.log(number2); 
+    var total = number1 + number2;
+    return total;
+}
+var firstNumber = 35;
+var secondNumber = 47;
+var result = addTwoNumbers(firstNumber, secondNumber);
+ console.log('result value:', result);
+
+//  function multiplyTwoNumbers(num1, num2){
+//      var result = num1 * num2;
+//      return result;
+//  }
+
+//  var total = multiplyTwoNumbers(5, 100);
+//  console.log('total after multiplication', total);
+
+//      Subtraction 
+ function subtraTwoNumbers(num1, num2){
+     var result = num1 - num2;
+     return result;
+ }
+
+ var total = subtraTwoNumbers(100, 10);
+ console.log('total after subtraction', total);
+ 
+ //    Division
+ function diviTwoNumbers(num1, num2){
+     var result = num1 / num2;
+     return result;
+ }
+
+ var total = diviTwoNumbers(100, 5);
+ console.log('total after division', total);
+
+ 
+ 
